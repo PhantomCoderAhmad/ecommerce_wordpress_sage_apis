85e13b9f76e4ce1aaa023fd29a21039ec69e38da
Ahmad <ahmadibrar0000@gmail.com>
2024-10-22 18:57:14 +0500

    issue #3 Header component changes according to theme layout

--- wp-content/themes/my_portfolio/resources/views/sections/header.blade.php
+++ wp-content/themes/my_portfolio/resources/views/sections/header.blade.php
@@ -1,29 +1,37 @@
-<header class="banner">
-  <nav class="navbar navbar-expand-sm bg-light fixed-top">
-    <div class="container">
-      <a class="navbar-brand" href="{{ home_url('/') }}">
-        {!! $siteName !!}
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+<!-- ======= Mobile nav toggle button ======= -->
+<i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        @if (has_nav_menu('primary_navigation'))
-          <ul class="navbar-nav mx-auto"> <!-- Changed from 'me-auto' to 'mx-auto' -->
-            {!! wp_nav_menu([
-                'theme_location' => 'primary_navigation',
-                'items_wrap' => '<ul class="navbar-nav mx-auto">%3$s</ul>',
-                'echo' => false
-            ]) !!}
+<!-- ======= Header ======= -->
+<header id="header">
+  <div class="d-flex flex-column">
 
-          </ul>
-        @endif
+    <div class="profile">
+      <img src="assets/img/profile_image.png" alt class="img-fluid rounded-circle">
+      <h1 class="text-light"><a href="index.html">Ahmad Yar</a></h1>
+      <p class="text-light text-center">I'm <span class="typed"
+          data-typed-items="Software Engineer, Full Stack Developer"></span>
+      </p>
 
-        
+      <div class="social-links mt-3 text-center">
+        <a href="javascript::void(0)" class="google-plus" title="ahmad.yar@786"><i class="bx bxl-skype"></i></a>
+        <a href="https://www.linkedin.com/in/ahmad-yar-9b3627178/" class="linkedin" target="_blank"><i
+            class="bx bxl-linkedin"></i></a>
       </div>
-      <div class="d-flex text-dark"><?php get_search_form(); ?></div>
-
     </div>
-  </nav>
-</header>
+
+    <nav id="navbar" class="nav-menu navbar">
+      @if (has_nav_menu('primary_navigation'))
+        <ul class="navbar-nav "> <!-- Changed from 'me-auto' to 'mx-auto' -->
+          {!! wp_nav_menu([
+              'theme_location' => 'primary_navigation',
+              'items_wrap' => '<ul class="navbar-nav mx-auto">%3$s</ul>',
+              'echo' => false
+          ]) !!}
+
+        </ul>
+      @endif
+    </nav><!-- .nav-menu -->
+  </div>
+</header><!-- End Header -->
+
+<!-- ======= Hero Section ======= -->
