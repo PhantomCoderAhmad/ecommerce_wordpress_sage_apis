ce54d462580e3407a10aa639804c626be91ef2e7
Ahmad <ahmadibrar0000@gmail.com>
2024-10-23 19:51:32 +0500

    issue #8 Implement Mail & Email Template for contact request

---
+++ wp-content/themes/my_portfolio/app/Mail/ContactRequestMail.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ContactRequestMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $contactData;
+
+    public function __construct($contactData)
+    {
+        $this->contactData = $contactData;
+    }
+
+    public function build()
+    {
+        return $this
+            ->subject('New Contact Request from ' . $this->contactData['name'])
+            ->view('emails.contact_request'); // Make sure to create this view
+    }
+}
